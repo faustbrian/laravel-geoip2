d32c01720b0a3eec7b57d88c690d962f1c8af49f
Brian Faust <hello@basecode.sh>
2023-03-14 08:00:12 +0200

    chore: organise tests directory

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PreemStudio\Jetpack\TestBench\AbstractPackageTestCase;
+
+abstract class TestCase extends AbstractPackageTestCase
+{
+    protected function getServiceProviderClass(): string
+    {
+        return \PreemStudio\GeoIp2\ServiceProvider::class;
+    }
+}
