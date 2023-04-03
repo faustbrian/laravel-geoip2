62b2381a94291c757e266bffa52cdc2124c63dae
Brian Faust <hello@basecode.sh>
2023-04-03 06:13:02 +0300

    chore: sync with skeleton repository

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,16 +8,16 @@ use PreemStudio\Jetpack\Package\AbstractServiceProvider;
 
 final class ServiceProvider extends AbstractServiceProvider
 {
-    public function bootingPackage(): void
-    {
-        $this->publishes([
-            __DIR__.'/../data/GeoLite2-City.mmdb'    => storage_path('app/GeoIp2/GeoLite2-City.mmdb'),
-            __DIR__.'/../data/GeoLite2-Country.mmdb' => storage_path('app/GeoIp2/GeoLite2-Country.mmdb'),
-        ], 'data');
-    }
-
-    public function register(): void
+    public function packageRegistered(): void
     {
         $this->app->singleton('geoip2', fn ($app) => new GeoIp2($app->config['laravel-geoip2']));
+    }
+
+    public function packageBooted(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../data/GeoLite2-City.mmdb'    => storage_path('app/GeoIp2/GeoLite2-City.mmdb'),
+            __DIR__ . '/../data/GeoLite2-Country.mmdb' => storage_path('app/GeoIp2/GeoLite2-Country.mmdb'),
+        ], 'data');
     }
 }
